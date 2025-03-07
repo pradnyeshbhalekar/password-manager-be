ac9a7bc9599e02ae5a68194e245b31b03c0dbfa8
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2025-03-08 00:01:53 +0530

    route problem

--- src/routes/passwordRoute.js
+++ src/routes/passwordRoute.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const {storePassword,getEncryptedPassword,getAllPassword,updatePassword,deletePassword} = require('../controller/passwordController')
+const {storePassword,getDecryptedPassword,getAllPassword,updatePassword,deletePassword} = require('../controller/passwordController')
 const {authMiddleware} = require('../middleware/authMiddleware')
 
 const router =express.Router();
 
 router.post("/",authMiddleware,storePassword)
 router.get('/',authMiddleware,getAllPassword)
-router.get('/:serviceId',authMiddleware,getEncryptedPassword)
+router.get('/:serviceId',authMiddleware,getDecryptedPassword)
 router.put('/update/:serviceId',authMiddleware,updatePassword);
 router.delete('/delete/:serviceId',authMiddleware,deletePassword);
 module.exports = router
